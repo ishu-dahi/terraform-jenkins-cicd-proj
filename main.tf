5ace6b377108a124b31937e993568e9a5acde08d
Ishu Dahiya <ishudahiya@Ishus-MacBook-Air.local>
2023-12-13 16:28:12 +0530

    terraform cicd initial push

--- main.tf
+++ main.tf
@@ -1,14 +1,31 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIARB3BHZBIMKBT4DU3"
-  secret_key = "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"
+  access_key = "AKIAWRUR7WKE7ZD54JEX"
+  secret_key = "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"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-02a2af70a66af6dfb"
+  ami           = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "jenkins-practical-terraform-server"
+    Name = "terraform-cicd-created-server"
+  }
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "172.30.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "terraform-cicd-created-vpc"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "ap-south-1a"
+  size              = 40
+
+  tags = {
+    Name = "terraform-created-ebs"
   }
 }
